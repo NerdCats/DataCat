00e94104f1b52ec95e8c836878aff71b8a798ecc
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-10 13:49:09 +0100

    Added some basic introductory code to go with

---
+++ src/DataCat.Core/QueryDocument.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace DataCat.Core
+{
+    public class QueryDocument
+    {
+        /// <summary>
+        /// Mongodb query object to query data from collection
+        /// </summary>
+        public JObject query { get; set; }
+
+        /// <summary>
+        /// Mongodb projection object to project data from the result
+        /// </summary>
+        public JObject project { get; set; }
+
+        /// <summary>
+        /// Mongodb aggregation pipeline to aggregate data from a collection
+        /// </summary>
+        public List<JObject> aggreagate { get; set; }
+    }
+}
---
+++ src/DataCat/Controllers/DataController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using DataCat.Core;
+
+namespace DataCat.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public void Post([FromBody] QueryDocument query)
+        {
+           
+        }
+    }
+}
