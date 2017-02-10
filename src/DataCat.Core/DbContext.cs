0e61f4751c1a5dbbf378ffe2edb82328bfdcef67
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-10 14:58:50 +0100

    Getting a very basic Database Context up. Need to access this result

--- src/DataCat.Core/DbContext.cs
+++ src/DataCat.Core/DbContext.cs
@@ -1,15 +1,31 @@
-﻿using MongoDB.Driver;
-
-namespace DataCat.Core
+﻿namespace DataCat.Core
 {
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using System;
+
     public class DbContext
     {
         private MongoClient mongoClient;
         public IMongoDatabase Database { get; private set; }
 
-        public DbContext()
+        public DbContext(string connectionString, string databaseName)
         {
-            
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException($"{nameof(connectionString)} is null or whitespace");
+
+            if (string.IsNullOrWhiteSpace(databaseName))
+                throw new ArgumentException($"{nameof(databaseName)} is null or whitespace");
+
+            var mongoUrlBuilder = new MongoUrlBuilder(connectionString);
+            mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
+
+            Database = mongoClient.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<BsonDocument> GetCollection(string collectionName)
+        {
+            return Database.GetCollection<BsonDocument>(collectionName);
         }
     }
 }
