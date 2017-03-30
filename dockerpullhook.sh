63a32c248ddd9774a77ae1f8431003caf4377b81
Swagata Prateek <thehoneymad@hotmail.com>
2017-03-31 00:43:03 +0200

    newlines are removed from the json payload

--- dockerpullhook.sh
+++ dockerpullhook.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-curl --request POST --url 'http://datacat.nerdcats.io:8080/pull?token=nerdcats' --header 'content-type: application/json' --data '{\n	"mode": "docker",\n	"image": "nerdcats/datacat:prod",\n	"arguments": "--name datacat -p 5000:5000"\n}'
+#!bin/bash
+curl --request POST --url 'http://datacat.nerdcats.io:8080/pull?token=nerdcats' --header 'content-type: application/json' --data '{"mode": "docker","image": "nerdcats/datacat:prod", "arguments": "--name datacat -p 5000:5000"}'
