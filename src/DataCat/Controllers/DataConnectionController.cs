321bac7807989971de964bf63434e0568d727680
Swagata Prateek <thehoneymad@hotmail.com>
2017-04-02 12:13:23 +0200

    Security works, yeehaw

--- src/DataCat/Controllers/DataConnectionController.cs
+++ src/DataCat/Controllers/DataConnectionController.cs
@@ -1,10 +1,16 @@
 ﻿namespace DataCat.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     [Route("api/[controller]")]
     public class DataConnectionController : Controller
     {
-
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Secured endpoint");
+        }
     }
 }
