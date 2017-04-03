7771da836e5ce2c92acb10344bf9badac90971d7
Swagata Prateek <thehoneymad@hotmail.com>
2017-04-03 22:54:58 +0200

    Added a global exception handler, hopefully just throwing exception will suffice now

---
+++ src/DataCat/ActionFilter/ServerExceptionFilter.cs
@@ -0,0 +1,45 @@
+﻿namespace DataCat.ActionFilter
+{
+    using DataCat.Core.Exception;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Net;
+
+    /// <summary>
+    /// Generic exception filter for unhandled exceptions and generating Http responses for it. 
+    /// Should never return a 500 Internal Server Error in production
+    /// </summary>
+    public class ServerExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception != null && context.HttpContext.Request != null)
+            {
+                var result = new ContentResult();
+                if (context.Exception is NotImplementedException)
+                    result.StatusCode = (int)HttpStatusCode.NotImplemented;
+                else if (context.Exception is EntityNotFoundException)
+                    result.StatusCode = (int)HttpStatusCode.NotFound;
+                else if (context.Exception is InvalidOperationException)
+                    result.StatusCode = (int)HttpStatusCode.Forbidden;
+                else if (context.Exception is ArgumentException)
+                    result.StatusCode = (int)HttpStatusCode.BadRequest;
+                else if (context.Exception is FormatException)
+                    result.StatusCode = (int)HttpStatusCode.BadRequest;
+                else if (context.Exception is ValidationException)
+                    result.StatusCode = (int)HttpStatusCode.BadRequest;
+                else if (context.Exception is NotSupportedException)
+                    result.StatusCode = (int)HttpStatusCode.BadRequest;
+                else if (context.Exception is UnauthorizedAccessException)
+                    result.StatusCode = (int)HttpStatusCode.Unauthorized;
+                else
+                    throw context.Exception;
+
+                result.ContentType = "application/json";
+                context.Result = result;
+            }
+        }
+    }
+}
