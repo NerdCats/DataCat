24736b6623e876f091f40e431f729d6cb07d5bf4
Swagata Prateek <thehoneymad@hotmail.com>
2017-04-09 05:18:47 +0200

    Pagination attribute adding

---
+++ src/DataCat/ActionFilter/PaginateAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace DataCat.ActionFilter
+{
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class PaginateAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
